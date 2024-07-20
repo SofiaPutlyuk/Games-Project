55c8833afa986c2c5540a179d7800ca2900c87b4
Sofia <putlyuksophia@gmail.com>
2024-07-20 11:47:57 +0300

    footer-game-project

---
+++ index.js
@@ -0,0 +1,32 @@
+/*Модальне вікно */
+const searchInputfooter = document.getElementById('footerInput')
+const searchInputButton = document.querySelector('.footer-input-container')
+searchInputButton.addEventListener('click', function () {
+    const inputFootervalue = searchInputfooter.value
+    if (inputFootervalue.includes('@')) {
+        const createModal = document.createElement('div')
+        createModal.classList.add('backdrop-footer')
+        createModal.innerHTML = `
+<div class="modal">
+<button  id="footerButton">
+<img src="Vector 234 (1).png" alt="button-close"/>
+</button>
+    <div class="modal-content">
+
+        <img src="photo-icon.png" alt="icon-game"/>
+        <p class="text-thanks">Дякую за підписку!</p>
+        <img src="photo-game.png" alt="icon2"/>
+    </div>
+</div>
+
+`
+        document.body.appendChild(createModal)
+        const searchFooterbutton = document.getElementById('footerButton')
+        console.log(searchFooterbutton)
+        searchFooterbutton.addEventListener('click', function () {
+            createModal.remove()
+            searchInputfooter.value = '';
+        })
+    }
+
+})
