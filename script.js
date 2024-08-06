fd835e94db337045b1c1f1d080d24b16c45f22b7
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-08-06 19:24:23 +0300

    fix class and format css/js

--- script.js
+++ script.js
@@ -11,44 +11,47 @@ const bthCalcAnswer = document.querySelector("#bth-calc-answer");
 let selectedOperation = null;
 
 function getNumbers() {
-    const numb1 = parseFloat(inputCalcNumb1.value);
-    const numb2 = parseFloat(inputCalcNumb2.value);
+  const numb1 = parseFloat(inputCalcNumb1.value);
+  const numb2 = parseFloat(inputCalcNumb2.value);
 
-    if (isNaN(numb1) || isNaN(numb2)) {
-        pCalcAnswer.textContent = "Error!";
-        return null;
-    }
-    return { numb1, numb2 };
+  if (isNaN(numb1) || isNaN(numb2)) {
+    pCalcAnswer.textContent = "Error!";
+    return null;
+  }
+  return { numb1, numb2 };
 }
 
-    function calculate() {
-        const numbers = getNumbers();
-        if (!numbers || !selectedOperation) return;
+function calculate() {
+  const numbers = getNumbers();
+  if (!numbers || !selectedOperation) return;
 
-        let answer;
-        switch (selectedOperation) {
-            case 'add':
-                answer = numbers.numb1 + numbers.numb2;
-                break;
-            case 'multiply':
-                answer = numbers.numb1 * numbers.numb2;
-                break;
-            case 'subtract':
-                answer = numbers.numb1 - numbers.numb2;
-                break;
-            case 'divide':
-                if (numbers.numb2 === 0) {
-                    pCalcAnswer.textContent = "Error!";
-                return;
-            }
-            answer = numbers.numb1 / numbers.numb2;
-            break;
-        }
-        pCalcAnswer.textContent = answer;
-    }
+  let answer;
+  switch (selectedOperation) {
+    case "add":
+      answer = numbers.numb1 + numbers.numb2;
+      break;
+    case "multiply":
+      answer = numbers.numb1 * numbers.numb2;
+      break;
+    case "subtract":
+      answer = numbers.numb1 - numbers.numb2;
+      break;
+    case "divide":
+      if (numbers.numb2 === 0) {
+        pCalcAnswer.textContent = "Error!";
+        return;
+      }
+      answer = numbers.numb1 / numbers.numb2;
+      break;
+  }
+  pCalcAnswer.textContent = answer;
+}
 
-bthCalcPlus.addEventListener("click", () => selectedOperation = 'add');
-bthCalcMultiply.addEventListener("click", () => selectedOperation = 'multiply');
-bthCalcMinus.addEventListener("click", () => selectedOperation = 'subtract');
-bthCalcDivide.addEventListener("click", () => selectedOperation = 'divide');
+bthCalcPlus.addEventListener("click", () => (selectedOperation = "add"));
+bthCalcMultiply.addEventListener(
+  "click",
+  () => (selectedOperation = "multiply")
+);
+bthCalcMinus.addEventListener("click", () => (selectedOperation = "subtract"));
+bthCalcDivide.addEventListener("click", () => (selectedOperation = "divide"));
 bthCalcAnswer.addEventListener("click", calculate);
