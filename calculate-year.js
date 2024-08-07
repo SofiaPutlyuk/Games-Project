e7ec5e402224a396e7f5081cbf92d85f6459ac15
Sofia <putlyuksophia@gmail.com>
2024-08-07 19:00:05 +0300

    fix/calculate-year

--- calculate-year.js
+++ calculate-year.js
@@ -7,23 +7,18 @@ searchicon.addEventListener('click', function () {
     if (existingMessage) {
         existingMessage.remove();
     }
-
+    const messageYear = document.createElement('p');
+    messageYear.id = "yearCheck";
     if (isNaN(searchValueYear) || searchInputYear.value.trim() === "") {
-        const messageYear = document.createElement('p');
-        messageYear.textContent = "Введіть рік";
-        messageYear.id = "yearCheck";
+       messageYear.textContent = "Будь ласка, вкажіть рік";
         messageYear.classList.add('result-input__red');
         document.querySelector('.box-input').appendChild(messageYear);
     } else if ((searchValueYear % 4 === 0 && searchValueYear % 100 !== 0) || (searchValueYear % 400 === 0)) {
-        const messageYear = document.createElement('p');
         messageYear.textContent = 'Ви народилися у високосний рік!';
-        messageYear.id = "yearCheck";
         messageYear.classList.add('result-input__green');
         document.querySelector('.box-input').appendChild(messageYear);
     } else {
-        const messageYear = document.createElement('p');
-        messageYear.textContent = 'Ви народилися не у високосний рік!';
-        messageYear.id = "yearCheck";
+       messageYear.textContent = 'Ви народилися не у високосний рік!';
         messageYear.classList.add('result-input__red');
         document.querySelector('.box-input').appendChild(messageYear);
     }
