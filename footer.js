2bb0a8e8e74a46bf6f358eeb747f9cf520ac1c09
Sofia <putlyuksophia@gmail.com>
2024-08-08 11:42:18 +0300

    fix/footer

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 /*Модальне вікно */
 const searchInputfooter = document.getElementById('footerInput')
-const searchInputButton = document.querySelector('.footer-input-container')
+const searchInputButton = document.querySelector('.footer-subscribe__button')
 searchInputButton.addEventListener('click', function () {
     const inputFootervalue = searchInputfooter.value
     if (inputFootervalue.includes('@')) {
@@ -9,13 +9,13 @@ searchInputButton.addEventListener('click', function () {
         createModal.innerHTML = `
 <div class="modal">
 <button  id="footerButton">
-<img src="scss/src/close-icon.png" alt="button-close"/>
+<img src="src/close-icon.png" alt="button-close"/>
 </button>
-    <div class="modal-content">
+    <div class="modal__content">
 
-        <img src="photo-icon.png" alt="icon-game"/>
+        <img src="src/photo-icon.png" alt="icon-game"/>
         <p class="text-thanks">Дякую за підписку!</p>
-        <img src="photo-game.png" alt="icon2"/>
+        <img src="src/photo-game.png" alt="icon2"/>
     </div>
 </div>
 
@@ -26,10 +26,10 @@ searchInputButton.addEventListener('click', function () {
         searchFooterbutton.addEventListener('click', function () {
             createModal.remove()
             searchInputfooter.value = '';
-            
+
         })
-        createModal.addEventListener('click',function(event){
-            if(event.target===createModal){
+        createModal.addEventListener('click', function (event) {
+            if (event.target === createModal) {
                 createModal.remove()
                 searchInputfooter.value = '';
             }
